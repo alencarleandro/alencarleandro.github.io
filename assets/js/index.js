f5bb031bab38b1e4a3ae69e02f4595e8c4772813
Leandro Alencar <149604111+alencarleandro@users.noreply.github.com>
2024-06-27 19:30:50 -0300

    Tela principal nao carrega quando inicia

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,57 +26,126 @@ const linkLinkedIn = document.querySelector("#linkLinkedIn");
 
 const cards_repos = document.querySelector("#cards_repos");
 
+const itensCarrosel = document.querySelector("#itensCarrosel");
+const contadorCarrosel = document.querySelector("#contadorCarrosel");
+
+const colegas = document.querySelector("#colegas");
+
+var dataUser;
+var dataRepos;
+var dataSocial;
+
+var dataS_Fotos;
+var dataS_Repo;
+var dataS_Reco;
+var dataS_Clg;
+
+/**
+ * Instanciando dados pela -> JsonServer - Vercel 
+ */
+
+async function getJsonServer_Fotos() {
+    fetch('https://site-pessoal-servidor.vercel.app/minhasFotos')
+        .then(async res => {
+            if (!res.ok) {
+                throw new Error(res.status);
+            }
+
+            dataS_Fotos = await res.json();
+
+        })
+}
+
+async function getJsonServer_Repositorios() {
+    fetch('https://site-pessoal-servidor.vercel.app/repositorios')
+        .then(async res => {
+            if (!res.ok) {
+                throw new Error(res.status);
+            }
+
+            dataS_Repo = await res.json();
+
+        })
+}
+
+async function getJsonServer_Recomendacoes() {
+    fetch('https://site-pessoal-servidor.vercel.app/recomendacoes')
+        .then(async res => {
+            if (!res.ok) {
+                throw new Error(res.status);
+            }
+
+            dataS_Reco = await res.json();
+
+        })
+}
+
+async function getJsonServer_Colegas() {
+    fetch('https://site-pessoal-servidor.vercel.app/colegas')
+        .then(async res => {
+            if (!res.ok) {
+                throw new Error(res.status);
+            }
+
+            dataS_Clg = await res.json();
+
+        })
+}
+
 /**
  * Instanciando dados pela -> GitHub - API 
  */
 
-function getApiGitHub_User() {
+async function getApiGitHub_User() {
     fetch('https://api.github.com/users/alencarleandro')
         .then(async res => {
             if (!res.ok) {
                 throw new Error(res.status);
             }
 
-            let data = await res.json();
-            txtNome.innerText = data.name;
-            txtDescricao.innerText = data.bio;
-            txtLocation.innerText += " " + data.location;
-            linkSite.href = data.blog;
-            linkGitHub.href = data.html_url;
-            txtNumeroSeguidores.innerText = data.followers
+            dataUser = await res.json();
+
+            txtNome.innerText = dataUser.name;
+            txtDescricao.innerText = dataUser.bio;
+            txtLocation.innerText += " " + dataUser.location;
+            linkSite.href = dataUser.blog;
+            linkGitHub.href = dataUser.html_url;
+            txtNumeroSeguidores.innerText = dataUser.followers
         })
 }
 
-function getApiGitHub_Repos() {
+async function getApiGitHub_Repos() {
     fetch('https://api.github.com/users/alencarleandro/repos')
         .then(async res => {
             if (!res.ok) {
                 throw new Error(res.status);
             }
 
-            let data = await res.json();
+            dataRepos = await res.json();
         })
 }
 
-function getApiGitHub_Social() {
+async function getApiGitHub_Social() {
     fetch('https://api.github.com/users/alencarleandro/social_accounts')
         .then(async res => {
             if (!res.ok) {
                 throw new Error(res.status);
             }
 
-            let data = await res.json();
-            linkInstagram.href = data[0].url;
-            linkLinkedIn.href = data[1].url;
+            dataSocial = await res.json();
+
+            linkInstagram.href = dataSocial[0].url;
+            linkLinkedIn.href = dataSocial[1].url;
+
         })
 }
-
 
 /**
  * Troca de paleta
  */
 
 function preto() {
+
     paleta.forEach((p) =>
         p.style.backgroundColor = "#000000"
     );
@@ -86,17 +155,22 @@ function preto() {
     fontEsp.forEach((txt) =>
         txt.style.color = "rgba(255, 255, 255, 0.8)"
     );
-    folha.style.backgroundColor = "rgba(255, 255, 255, 0.3)"
-    seletorPaleta.style.backgroundColor = "rgba(255, 255, 255, 0.4)"
-    eu.style.backgroundImage = "url(\"./assets/img/eu5.jpeg\")";
+
+    instanciarColegas("text_paleta_02");
+
+    folha.style.backgroundColor = "rgba(255, 255, 255, 0.3)";
+    seletorPaleta.style.backgroundColor = "rgba(255, 255, 255, 0.4)";
     instagram.src = "./assets/img/instagramBranco.png";
     linkedin.src = "./assets/img/linkedinBranco.png";
     seguidor.src = "./assets/img/seguidor_branco.png";
 
-    pendurar_card_repo("./assets/img/LogoVazia.png","Legnu","teste","1","2","color_paleta_01");
+    eu.style.backgroundImage = `url(${dataS_Fotos[0].foto})`;
+
+    instanciarRepositorios("color_paleta_01");
 }
 
-function azulMarinho(){
+function azulMarinho() {
+
     paleta.forEach((p) =>
         p.style.backgroundColor = "#01355c"
     );
@@ -106,20 +180,23 @@ function azulMarinho(){
     fontEsp.forEach((txt) =>
         txt.style.color = "rgba(255, 255, 255, 1)"
     );
-    seletorPaleta.style.backgroundColor = "rgba(255, 255, 255, 0.5)"
-    folha.style.backgroundColor = "rgba(0, 0, 0, 0.4)"
-    eu.style.backgroundImage = "url(\"./assets/img/eu2.png\")";
+
+    seletorPaleta.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+    folha.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
     instagram.src = "./assets/img/instagramBranco.png";
     linkedin.src = "./assets/img/linkedinBranco.png";
     seguidor.src = "./assets/img/seguidor_branco.png";
 
-    for(i = 0; i< 10;i++){
-        pendurar_card_repo("./assets/img/LogoVazia.png","Legnu","teste","1","2","color_paleta_02");
+    instanciarColegas("text_paleta_02");
 
-    }
+    instanciarRepositorios("color_paleta_02");
+
+    eu.style.backgroundImage = `url(${dataUser.avatar_url})`;
+
 }
 
 function beje() {
+
     paleta.forEach((p) =>
         p.style.backgroundColor = "#e7d6b9"
     );
@@ -129,34 +206,99 @@ function beje() {
     fontEsp.forEach((txt) =>
         txt.style.color = "rgba(0, 0, 0, 0.8)"
     );
+
     folha.style.backgroundColor = "rgba(255, 255, 255, 0.7)"
     seletorPaleta.style.backgroundColor = "rgba(0, 0, 0, 0.3)"
-    eu.style.backgroundImage = "url(\"./assets/img/eu3.png\")";
     instagram.src = "./assets/img/instagram.png";
     linkedin.src = "./assets/img/linkedin.png";
     seguidor.src = "./assets/img/seguidor.png";
 
-    pendurar_card_repo("./assets/img/LogoVazia.png","Legnu","teste","1","2","color_paleta_03");
+    eu.style.backgroundImage = `url(${dataS_Fotos[1].foto})`; 
+
+    instanciarColegas("text_paleta_01");
+    
+    instanciarRecomendacoes();   
+    instanciarRepositorios("color_paleta_03");
 }
+
+function start() {
+
+    azulMarinho();
+};
 
 window.addEventListener("load", () => {
     getApiGitHub_User();
     getApiGitHub_Repos();
     getApiGitHub_Social();
 
-    azulMarinho();
+    getJsonServer_Fotos();
+    getJsonServer_Repositorios();
+    getJsonServer_Recomendacoes();
+    getJsonServer_Colegas();
 });
+
+/**
+ * Instanciar dados
+ */
+
+function instanciarRepositorios(paleta) {
+
+    cards_repos.innerHTML = "";
+
+    for (i = 0; i < dataS_Repo.length; i++) {
+
+        if (!dataS_Repo[i].link == "") {
+            for (j = 0; j < dataRepos.length; j++) {
+                if (dataS_Repo[i].link == dataRepos[j].html_url) {
+                    pendurar_card_repo(dataS_Repo[i].foto, dataS_Repo[i].titulo, dataS_Repo[i].descricao, dataRepos[j].stargazers_count, dataRepos[j].watchers_count, paleta)
+                }
+            }
+        } else {
+            pendurar_card_repo(dataS_Repo[i].foto, dataS_Repo[i].titulo, dataS_Repo[i].descricao, "0", "0", paleta)
+        }
+
+    }
+
+}
+
+function instanciarRecomendacoes() {
+
+    itensCarrosel.innerHTML = "";
+    contadorCarrosel.innerHTML = "";
+
+    for (i = 0; i < dataS_Reco.length; i++) {
+
+        if(i == 0){
+            pendurar_card_reco(dataS_Reco[i].imagem, dataS_Reco[i].link, i, "active")
+        }else{
+            pendurar_card_reco(dataS_Reco[i].imagem, dataS_Reco[i].link, i, "estado")
+        }
+
+    }
+
+}
+
+function instanciarColegas(paleta) {
+
+    colegas.innerHTML = "";
+
+    for (i = 0; i < dataS_Clg.length; i++) {
+        pendurar_card_colega(dataS_Clg[i].foto, dataS_Clg[i].link, dataS_Clg[i].nome, paleta);
+
+    }
+
+}
 
 /**
  * Elementos
  */
 
 function pendurar_card_repo(img, titulo, descricao, favoritos, forks, paleta) {
-  
-    let card = 
+
+    let card =
         `<div class="card p-4 m-3 centro cursorHand repo">
             <div class="${paleta} x100x300 radius_All_-0-" style="display: flex; align-items: center; justify-content: center;">
-                <img class="x100x300 ra" src="${img}" alt="Card image cap">
+                <img class="h100 ra" src="${img}" alt="Card image cap">
             </div>
 
             <div class="card-body hM300 h200">
@@ -180,4 +322,44 @@ function pendurar_card_repo(img, titulo, descricao, favoritos, forks, paleta) {
     const x = document.createElement('div');
     x.innerHTML = card;
     cards_repos.appendChild(x);
+}
+
+function pendurar_card_reco(img, link, n, estado) {
+
+    let card =
+        `<div class="carousel-item ${estado}">
+            <a href="${link}">
+                <img class="d-block w-100" src="${img}">
+            </a>
+            <div class="carousel-caption d-none d-md-block"></div>
+         </div>`;
+
+    const x = document.createElement('div');
+    x.innerHTML = card;
+    itensCarrosel.appendChild(x);
+
+    let card2 =
+        `<button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="${n}"
+                                class="${estado}" aria-current="true"></button>`;
+
+    const y = document.createElement('div');
+    y.innerHTML = card2;
+    contadorCarrosel.appendChild(y);
+}
+
+function pendurar_card_colega(img, link, nome, paleta) {
+
+    let card =
+        `<div class="centro ">
+            <a class="textUnd" href="${link}">
+                <span class="textoNome textUnd">
+                    <img class="rounded-circle colegasTrabalho textUnd" src="${img}" alt="${nome}, foto perfil.">
+                    <div class="mt-3 textUnd ${paleta}"><strong>${nome}</strong></div>
+                </span>
+            </a>
+        </div>`;
+
+    const x = document.createElement('div');
+    x.innerHTML = card;
+    colegas.appendChild(x);
 }
